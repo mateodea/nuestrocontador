fbda0eef9db981d70806a233fafb1231066cea10
mateodea <mateodea@live.com>
2025-06-18 00:17:34 -0300

    Add files via upload

---
+++ tu-contador (5)/next.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+  distDir: 'out',
+  basePath: '',
+  assetPrefix: '',
+  images: {
+    unoptimized: true
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+}
+
+export default nextConfig
